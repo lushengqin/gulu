d1a8b986bf0e0b5a5e2d22cd1f78a18f6580a039
“e <2015941587@qq.com>
2020-06-01 20:28:09 +0800

    添加click事件

--- src/button.vue
+++ src/button.vue
@@ -1,34 +1,43 @@
 <template>
-    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-        <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
-        <g-icon class="loading" name="loading"></g-icon>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click')" >
+        <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
+        <g-icon class="loading icon" v-if="loading " name="loading"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
     </button>
 </template>
 <script>
-	export default{
-        // props:['icon','iconPosition']
-        props:{
-        	icon:{},
-	        iconPosition:{
-	            type: String,
-	            default:'left',
-	            validator(value){
-		            return value !== 'left' && value !== 'right' ? false : true;
-	            }
-            }
-        }
+	export default {
+		// props:['icon','iconPosition']
+		props: {
+			icon: {},
+			loading: {
+				type: Boolean,
+				default: false
+			},
+			iconPosition: {
+				type: String,
+				default: 'left',
+				validator(value) {
+					return value !== 'left' && value !== 'right' ? false : true;
+				}
+			}
+		}
 	}
 </script>
 <style lang="scss">
     @keyframes iLoading {
-        0%{transform: rotate(0deg)};
-        100%{transform: rotate(360deg)}
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
 
     }
-    .g-button{
+
+    .g-button {
         font-size: var(--font-size);
         height: var(--button-height);
         padding: 0 .9em;
@@ -39,34 +48,42 @@
         justify-content: center;
         align-items: center;
         vertical-align: middle;
-        &:hover{
-        border-color: var(--border-color-hover);
+
+        &:hover {
+            border-color: var(--border-color-hover);
         }
-        &:active{
+
+        &:active {
             background-color: var(--button-active-background);
         }
-        &:focus{
+
+        &:focus {
             outline: none;
         }
-        > .content{
+
+        > .content {
             order: 2;
         }
-        > .icon{
+
+        > .icon {
             order: 1;
             margin-right: .2em;
         }
-        &.icon-right{
-            > .content{
+
+        &.icon-right {
+            > .content {
                 order: 1;
             }
-            > .icon{
-                order:2;
+
+            > .icon {
+                order: 2;
                 margin-left: .2em;
                 margin-right: 0;
             }
         }
-        .loading{
-            animation:iLoading 3s infinite linear;
+
+        .loading {
+            animation: iLoading 3s infinite linear;
         }
     }
 
